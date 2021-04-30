266002101428d1023522fa1cc1918a8a7560af41
Krissada Sarawit <topfeekrissada@outlook.com>
2021-05-01 02:32:29 +0700

    Finished

--- KindAlgo/CheckKind.java
+++ KindAlgo/CheckKind.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Arrays;
 
 public class CheckKind {
 
     public static void main(String[] args) {
 
         ArrayList<Integer> arr1 = new ArrayList<Integer>();
-        arr1.add(1);
-        arr1.add(3);
-        arr1.add(4);
-        arr1.add(5);
-        arr1.add(7);
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+
+        arr1.addAll(Arrays.asList(1, 3, 4, 5, 7));
+
+        arr2.addAll(Arrays.asList(1, 2, 3, 3, 3, 4, 4, 4, 6, 7, 9, 4, 5));
 
         Collections.sort(arr1);
+        Collections.sort(arr2);
 
         System.out.println(arr1);
+
+        System.out.println("HAS KIND::" + hasKind(arr1));
 
     }
 
@@ -23,17 +27,34 @@ public class CheckKind {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         Collections.sort(arr);
 
-        /*  */
+        int indexOfSubArray = 0;
         int kindCount = 0;
+        boolean isContinue = false;
 
         for (int i = 0; i < arr.size() - 1; i++) {
             if (arr.get(i) == arr.get(i + 1)) {
                 kindCount++;
+                isContinue = true;
+            } else {
+                /* No more continue. */
+                if (isContinue && kindCount >= 2) {
+                    /* They are kind. */
+                    result.add(new ArrayList<Integer>());
+                    /* Add the continue cards into the result array */
+                    for (int k = kindCount + 1; k > 0 + 1; k--) {
+                        /* Add index in the sub array */
+                        result.get(indexOfSubArray).add(i - k);
+                    }
+                    indexOfSubArray++;
+                }
+
+                isContinue = false;
+                kindCount = 0;
             }
 
         }
 
-        return result;
+        return (result.isEmpty()) ? null : result;
 
     }
 
