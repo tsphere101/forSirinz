2687f8ba95b9b1d3806df6f472d5c19ad53ca2bd
Krissada Sarawit <topfeekrissada@outlook.com>
2021-05-02 02:08:21 +0700

    refactoring : rename variables and remove degub print

--- GinRummyAlgorithm/BotIntellisense/BotIntelligent.java
+++ GinRummyAlgorithm/BotIntellisense/BotIntelligent.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class BotIntelligent {
@@ -35,11 +34,12 @@ public class BotIntelligent {
         arr3.add(new MildCard(3, "s"));
         arr3.add(new MildCard(1, "d"));
 
-        ArrayList<MildCard> arr4= new ArrayList<MildCard>();
-        arr4.add(new MildCard(5, "c"));
-        arr4.add(new MildCard(7, "c"));
-        arr4.add(new MildCard(6, "s"));
+        ArrayList<MildCard> arr4 = new ArrayList<MildCard>();
         arr4.add(new MildCard(8, "d"));
+        arr4.add(new MildCard(9, "d"));
+        arr4.add(new MildCard(3, "h"));
+        arr4.add(new MildCard(9, "c"));
+        arr4.add(new MildCard(7, "s"));
 
         System.out.println(botIntelliDrop(arr4).getValue() + "SUIT::" + botIntelliDrop(arr4).getSuit());
 
@@ -50,17 +50,9 @@ public class BotIntelligent {
         Collections.sort(arr);
         ArrayList<MildCard> duplicate = new ArrayList<MildCard>();
         ArrayList<MildCard> duplicateForNull = new ArrayList<MildCard>();
-        // for (int i = 0; i < duplicate.size(); i++) {
-        //     duplicate.add(arr.get(i));
-        //     duplicateForNull.add(arr.get(i));
-        // }
 
         duplicate = (ArrayList<MildCard>) arr.clone();
         duplicateForNull = (ArrayList<MildCard>) arr.clone();
-        
-        System.out.println("ARR ARRAYLIST SIZE :: " + arr.size());
-        System.out.println("DUPLICATE ARRAYLIST SIZE :: " + duplicate.size());
-        System.out.println("DUPLICATEFORNULL ARRAYLIST SIZE :: " + duplicateForNull.size());
 
         int i = duplicate.size() - 1;
         while (i > 0) {
@@ -74,46 +66,12 @@ public class BotIntelligent {
                 duplicate.remove(i - 1);
                 i--;
             }
-
             i--;
-            if (i <= 0) {
-                System.out.println("CRITICAl VALUE OF I :: " + i);
-            }
         }
-
         if (duplicate.isEmpty()) {
-            System.out.println("ALL IN HAND(TSPEHRE)");
             return duplicateForNull.get(duplicateForNull.size() - 1);
         }
-
         return duplicate.get(duplicate.size() - 1);
-
-    }
-
-    public static MildCard botIntelliDropNEEDTOFIXBUG(ArrayList<MildCard> arr) {
-        Collections.sort(arr); /* Sort by deadwoodRank */
-        System.out.println(arr);
-
-        for (int i = arr.size() - 1; i >= 1; i--) {
-            if (arr.get(i).getValue() != arr.get(i - 1).getValue()) {
-                if (!arr.get(i).getSuit().equals(arr.get(i - 1).getSuit())) {
-                    System.out.println(arr.get(i));
-                    return arr.get(i); // NO FRIEND, NO BROTHER
-                } else {
-                    // HAS 1 BROTHER (SAME SUIT)
-                    i--;
-                }
-
-            } else {
-                // HAS 1 FRIEND (SAME RANK)
-                i--;
-            }
-        }
-
-        System.out.println("BOTINTELLIDROP RETURNED NULL");
-
-        return null;
-
     }
 
 }
